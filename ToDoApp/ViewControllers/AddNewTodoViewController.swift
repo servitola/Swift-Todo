d942a0dc8fbef98b74d0cfd6c9dfbcf6ae0fb0ec
servitola <servitola@gmail.com>
2022-10-06 21:31:16 +0300

    сохранять введенное в поле значение в память телефона

--- ToDoApp/ViewControllers/AddNewTodoViewController.swift
+++ ToDoApp/ViewControllers/AddNewTodoViewController.swift
@@ -3,12 +3,17 @@ import UIKit
 class AddNewTodoViewController: UIViewController {
     
     @IBAction func OnAddNewToDoButtonClick() {
+
+        let defaults = UserDefaults.standard
+        defaults.set(ToDoTextField.text!, forKey: "todo items")
         
-        let alertController = UIAlertController(title: "Default Style", message: "A standard alert.", preferredStyle: .alert)
+        let alertController = UIAlertController(title: "Hooray", message: "Todo with text \(ToDoTextField.text!) was saved", preferredStyle: .alert)
 
         let OKAction = UIAlertAction(title: "OK", style: .default)
         alertController.addAction(OKAction)
         
         present(alertController, animated: true)
     }
+    
+    @IBOutlet weak var ToDoTextField: UITextField!
 }
