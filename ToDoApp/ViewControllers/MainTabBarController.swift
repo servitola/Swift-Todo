5fecfc1210cecfc9363628d9a45025be2c0b7dab
servitola <servitola@gmail.com>
2022-10-06 22:16:31 +0300

    сделать из 1-ой страницы верстку вместо кода

--- ToDoApp/ViewControllers/MainTabBarController.swift
+++ ToDoApp/ViewControllers/MainTabBarController.swift
@@ -5,7 +5,7 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let toDoListViewController = ToDoListViewController()
+        let toDoListViewController = UIStoryboard(name: "ToDoListViewController", bundle: Bundle.main).instantiateInitialViewController()!
         toDoListViewController.tabBarItem = UITabBarItem(title: "Делать", image: UIImage(systemName: "house"), selectedImage: nil)
         
         let addNewToDoViewController = UIStoryboard(name: "AddNewTodoViewController", bundle: Bundle.main).instantiateInitialViewController()!
--- ToDoApp/ViewControllers/ToDoListViewController.swift
+++ ToDoApp/ViewControllers/ToDoListViewController.swift
@@ -5,35 +5,13 @@ class ToDoListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.backgroundColor = UIColor.systemBackground
-
-        let scrollView = UIScrollView()
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        
-        let verticalStackView = UIStackView()
-        verticalStackView.axis = .vertical
-        verticalStackView.spacing = 10
-        verticalStackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        view.addSubview(scrollView)
-        scrollView.addSubview(verticalStackView)
-        
         for _ in 1...50 {
             let labelView = UILabel()
             labelView.text = "Следующая вещь на моем пути"
             labelView.backgroundColor = UIColor.red
-            verticalStackView.addArrangedSubview(labelView)
+            ContentStackView.addArrangedSubview(labelView)
         }
-        
-        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
-        scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
-        scrollView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-        scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
-
-        verticalStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
-        verticalStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
-        verticalStackView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
-        verticalStackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
-        verticalStackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
     }
+    
+    @IBOutlet weak var ContentStackView: UIStackView!
 }
