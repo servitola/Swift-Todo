6ca27dfba543c499ef11b7a63937ef2a03ea0ff4
servitola <servitola@gmail.com>
2022-10-06 19:58:05 +0300

    добавить на 1-ю страницу список лейблов с текстом, которые можно скроллить

--- ToDoApp/ViewControllers/Tab1ViewController.swift
+++ ToDoApp/ViewControllers/Tab1ViewController.swift
@@ -4,6 +4,36 @@ class Tab1ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         self.view.backgroundColor = UIColor.systemBackground
+
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let verticalStackView = UIStackView()
+        verticalStackView.axis = .vertical
+        verticalStackView.spacing = 10
+        verticalStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        view.addSubview(scrollView)
+        scrollView.addSubview(verticalStackView)
+        
+        for _ in 1...50 {
+            let labelView = UILabel()
+            labelView.text = "Следующая вещь на моем пути"
+            labelView.backgroundColor = UIColor.red
+            verticalStackView.addArrangedSubview(labelView)
+        }
+        
+        scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
+        scrollView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+
+        verticalStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor).isActive = true
+        verticalStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor).isActive = true
+        verticalStackView.topAnchor.constraint(equalTo: scrollView.topAnchor).isActive = true
+        verticalStackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
+        verticalStackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor).isActive = true
     }
 }
