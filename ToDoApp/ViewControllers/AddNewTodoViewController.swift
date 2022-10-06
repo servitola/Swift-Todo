f5dba9d7a9dda1e594047500b3596faf2531915d
servitola <servitola@gmail.com>
2022-10-06 21:19:22 +0300

    добавить обработчик клика на кнопке “добавить” и тестовый диалог, чтобы увидеть, что кнопка сработала

--- ToDoApp/ViewControllers/AddNewTodoViewController.swift
+++ ToDoApp/ViewControllers/AddNewTodoViewController.swift
@@ -1,6 +1,14 @@
 import UIKit
 
 class AddNewTodoViewController: UIViewController {
-
     
+    @IBAction func OnAddNewToDoButtonClick() {
+        
+        let alertController = UIAlertController(title: "Default Style", message: "A standard alert.", preferredStyle: .alert)
+
+        let OKAction = UIAlertAction(title: "OK", style: .default)
+        alertController.addAction(OKAction)
+        
+        present(alertController, animated: true)
+    }
 }
