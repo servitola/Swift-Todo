38d6cdf061fe55bd6468f4e1336818c069acad1c
vkonovalov <vkonovalov@spotware.com>
2022-10-06 17:18:41 +0300

    удалить бесполезные комментарии и перегрузки методов, которые не делают ничего

--- ToDoApp/ViewController.swift
+++ ToDoApp/ViewController.swift
@@ -1,19 +1,6 @@
-//
-//  ViewController.swift
-//  ToDoApp
-//
-//  Created by Vlad Konovalov on 05/10/2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
 
 }
 
