8f2131d4db02995f2320600ff6c7fabb746c4bd2
vkonovalov <vkonovalov@spotware.com>
2022-10-06 17:19:28 +0300

    показать первый пустой экран

--- ToDoApp/SceneDelegate.swift
+++ ToDoApp/SceneDelegate.swift
@@ -7,7 +7,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
    
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+                
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = UIViewController()
+        window?.makeKeyAndVisible()
     }
 }
 
