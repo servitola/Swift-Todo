9b9eacb6c86aa71794139c22a47e6541c970663a
servitola <servitola@gmail.com>
2022-10-06 22:33:52 +0300

    показывать текст из сохраненной в память телефона ToDo

--- ToDoApp/ViewControllers/ToDoListViewController.swift
+++ ToDoApp/ViewControllers/ToDoListViewController.swift
@@ -1,13 +1,16 @@
 import UIKit
 
 class ToDoListViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        for _ in 1...50 {
+        ContentStackView.arrangedSubviews.forEach({ ContentStackView.removeArrangedSubview($0)})
+        
+        let defaults = UserDefaults.standard
+        if let todoItems = defaults.string(forKey: "todo items") {
             let labelView = UILabel()
-            labelView.text = "Следующая вещь на моем пути"
+            labelView.text = todoItems
             labelView.backgroundColor = UIColor.red
             ContentStackView.addArrangedSubview(labelView)
         }
