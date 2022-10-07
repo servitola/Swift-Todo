0112354e54bfa6d80e1f861633be46f717834bb7
servitola <servitola@gmail.com>
2022-10-08 00:11:21 +0300

    облагородить карточку ToDo

--- ToDoApp/Views/ToDoCardView.swift
+++ ToDoApp/Views/ToDoCardView.swift
@@ -18,28 +18,34 @@ class ToDoCardView : UIView
     func initView(toDo: ToDo)
     {
         label.text = toDo.title
-        label.backgroundColor = UIColor.systemBackground
+        label.layoutMargins = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         
-        deleteButton.backgroundColor = .green
-        deleteButton.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        deleteButton.tintColor = .label
+        deleteButton.setImage(UIImage(systemName: "trash"), for: UIControl.State.normal)
+        deleteButton.widthAnchor.constraint(equalToConstant: 60).isActive = true
         deleteButton.addTarget(self, action: #selector(onDeleteButtonClicked), for: .touchUpInside)
-       
+        
         if toDo.isDone {
             deleteButton.isHidden = true
         }
         
         let stackView = UIStackView()
+        stackView.spacing = 10
+        stackView.backgroundColor = .systemMint
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .horizontal
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.layoutMargins = UIEdgeInsets(top: 8, left: 8, bottom: 8, right: 8)
+        
         stackView.addArrangedSubview(label)
         stackView.addArrangedSubview(deleteButton)
-        deleteButton.setImage(UIImage(systemName: "minus"), for: UIControl.State.normal)
+        
         addSubview(stackView)
         
-        stackView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
-        stackView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        stackView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
-        stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+        stackView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        stackView.topAnchor.constraint(equalTo: self.topAnchor, constant: 8).isActive = true
+        stackView.rightAnchor.constraint(equalTo: self.rightAnchor, constant: -8).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 8).isActive = true
         
         self.heightAnchor.constraint(equalToConstant: 50).isActive = true
     }
